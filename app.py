089f6f091eafee58fe7a512be20455fbcae4540a
EStherKiguru <esipraisewambui@gmail.com>
2024-09-10 21:35:40 +0300

    Add main FastAPI app entry point

---
+++ app.py
@@ -0,0 +1,22 @@
+"""
+Initializes and configures the FastAPI application
+Sets up the app instance and integrates components like and database connections
+"""
+from fastapi import FastAPI
+from db import engine, Base
+from controller import router as api_router
+from fastapi.staticfiles import StaticFiles
+
+# Initialize FastAPI app
+StatSync = FastAPI()
+
+# Mount the static directory for serving static files
+StatSync.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Include the API router with the routes
+StatSync.include_router(api_router)
+
+# Create the database tables
+Base.metadata.create_all(bind=engine)
+
+print("Database and tables created successfully.")
