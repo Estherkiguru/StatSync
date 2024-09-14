e6f83a8862b18cb20b4d832b078e9c44e2b289ed
EStherKiguru <esipraisewambui@gmail.com>
2024-09-15 00:47:57 +0300

    Define settings and configuration for environment variables

---
+++ config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    class Config:
+        env_file = ".env" 
+
+# Create an instance of the Settings class
+settings = Settings()
