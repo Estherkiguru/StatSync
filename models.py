fb5f7966509b7b93b228227f49d13333a5956e86
EStherKiguru <esipraisewambui@gmail.com>
2024-09-15 00:37:14 +0300

    Add Trainer model, foreign key relationship, and update Athlete model

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, Date
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 from db import Base
 
 class Athlete(Base):
@@ -15,7 +17,28 @@ class Athlete(Base):
     email = Column(String, unique=True, index=True)
     password = Column(String)
 
+    # Fields to be field out by the Trainer
     body_weight = Column(Float, nullable=True)
     bmr = Column(Float, nullable=True)
     hydration_level = Column(Float, nullable=True)
     muscle_mass = Column(Float, nullable=True)
+
+    # Add a foreign key to link to the Trainer model
+    trainer_id = Column(Integer, ForeignKey('trainers.id'))
+
+    # Create a relationship with Trainer
+    trainer = relationship('Trainer', back_populates='athletes')
+class Trainer(Base):
+    __tablename__ = 'trainers'
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    gender = Column(String)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    username = Column(String, unique=True, index=True)
+    date_of_birth = Column(Date)
+
+    # Create the relationship with Athlete
+    athletes = relationship('Athlete', back_populates='trainer')
